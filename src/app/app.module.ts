5dc48c24b6218dfcf4220dfafac55a380e9a9e2e
Christina Demertzi <demertzixris@gmail.com>
2020-12-01 14:55:17 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WrapperComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
